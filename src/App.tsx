6bd689cdbb7b0f6908653a592e81064f38a39a96
Sandeep <sandeepmenta172@gmail.com>
2021-10-20 08:25:45 +0530

    changes related to time interval added for every 30 seconds it will call the apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
-import './App.scss';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import "./App.scss";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import { fetchHighLightsData, fetchIncomeData, fetchCountriesData, fetchBuyersData } from './redux/actions/app.actions';
-import Header from './common/Header';
-import Card from './common/Card';
+import {
+  fetchHighLightsData,
+  fetchIncomeData,
+  fetchCountriesData,
+  fetchBuyersData,
+} from "./redux/actions/app.actions";
+import Header from "./common/Header";
+import Card from "./common/Card";
 function App() {
   const dispatch = useDispatch();
-  const { income, highLights, countries, buyers, isLoading } = useSelector((state: any) => state.appReducer);
+  const { income, highLights, countries, buyers, isLoading } = useSelector(
+    (state: any) => state.appReducer
+  );
 
   useEffect(() => {
     dispatch(fetchHighLightsData('get_highlight'));
     dispatch(fetchIncomeData('get_income'));
     dispatch(fetchCountriesData('get_country'));
     dispatch(fetchBuyersData('get_buyer'));
-  }, [dispatch]);
+    const timer = setInterval(() => {
+      dispatch(fetchHighLightsData("get_highlight"));
+      dispatch(fetchIncomeData("get_income"));
+      dispatch(fetchCountriesData("get_country"));
+      dispatch(fetchBuyersData("get_buyer"));
+    }, 30000);
+    return() => {
+      clearInterval(timer);
+    }
+  }, []);
 
   return (
     <div className="App">
       <Header />
-      {isLoading ? <h1>Loading...</h1> : <section>
-      <Card headerTitle="Highlights" data={highLights} />
-      <Card headerTitle="Buyers" data={buyers} />
-      <Card headerTitle="Countries" data={countries} />
-      <Card headerTitle="Income" data={income} />
-      </section>}
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <section>
+          <Card headerTitle="Highlights" data={highLights} />
+          <Card headerTitle="Buyers" data={buyers} />
+          <Card headerTitle="Countries" data={countries} />
+          <Card headerTitle="Income" data={income} />
+        </section>
+      )}
     </div>
   );
 }
